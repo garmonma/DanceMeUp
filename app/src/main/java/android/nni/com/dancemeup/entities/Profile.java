66b6528f5aa88d52286ab8cc1d35dcae00c451f8
garmonma <garmonmarcus@gmail.com>
2017-12-22 17:14:46 -0700

    Started Fragments for registration page

---
+++ app/src/main/java/android/nni/com/dancemeup/entities/Profile.java
@@ -0,0 +1,22 @@
+package android.nni.com.dancemeup.entities;
+
+/**
+ * Created by magma on 12/22/2017.
+ */
+
+public class Profile {
+
+    private String email;
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public Profile(){
+
+    }
+}
---
+++ app/src/main/java/android/nni/com/dancemeup/entities/User.java
@@ -0,0 +1,17 @@
+package android.nni.com.dancemeup.entities;
+
+/**
+ * Created by magma on 12/22/2017.
+ */
+
+public class User {
+
+    private String username;
+
+    private String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+}
